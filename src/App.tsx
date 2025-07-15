0cb693dd521b815517f8d2aa5bbe552509b2ba7b
CodeSandbox <support@codesandbox.io>
2025-07-15 21:04:15 +0000

    Sandbox snapshot commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import Checklist from "./checklist";
 
 export default function App() {
   return (
-    <main className="bg-gray-100 min-h-screen py-10">
+    
       <Checklist />
-    </main>
+   
   );
 }
